81737923388d890d72c235fd9b9bd081355cefb4
Jason Voegele <jason@jvoegele.com>
2010-04-01 15:59:30 -0400

    Refactored AndroidPlugin class.
    * Use instance variables to hold references to project and task
      instances.
    * Define each task in its own method.
    * Create stubs for tasks that are not yet implemented.
    * Make lifecycle more explicit by defining all task dependencies
      together in a single method.

--- src/main/groovy/com/jvoegele/gradle/plugins/android/AndroidPlugin.groovy
+++ src/main/groovy/com/jvoegele/gradle/plugins/android/AndroidPlugin.groovy
@@ -13,110 +13,97 @@ import com.jvoegele.gradle.tasks.android.ProcessAndroidResources
  *
  * @author Jason Voegele (jason@jvoegele.com)
  */
-
-/*
-android-plugin tasks
---------------------
-generateSources
-    Generates the R.java source file used to access resources
-    dependants: compileJava
-
-proguard
-    Processes classes and JARs with ProGuard
-    dependsOn: classes
-
-androidPackageDebug
-    Creates the Android application apk package, signed with debug key
-    dependsOn: classes
-    dependants: assemble
-
-androidPackageRelease
-    Creates the Android application apk package, which must be signed before it is published
-    dependsOn: classes
-
-androidInstall
-    Installs the debug package onto a running emulator or device
-    dependsOn: androidDebugPackage
-
-androidUninstall
-    Uninstalls the application from a running emulator or device
-
-Should proguard task be exposed?  I think so, since that will allow it to be configured or disabled.
-*/
 class AndroidPlugin implements Plugin {
-  private static final String GENERATE_SOURCES_TASK_NAME = "generateSources"
-  private static final String PROGUARD_TASK_NAME = "proguard"
-  private static final String ANDROID_PACKAGE_DEBUG_TASK_NAME = "androidPackageDebug"
-  private static final String ANDROID_PACKAGE_RELEASE_TASK_NAME = "androidPackageRelease"
-  private static final String ANDROID_INSTALL_TASK_NAME = "androidInstall"
-  private static final String ANDROID_UNINSTALL_TASK_NAME = "androidUninstall"
-
+  private static final ANDROID_PROCESS_RESOURCES_TASK_NAME = "androidProcessResources"
+  private static final PROGUARD_TASK_NAME = "proguard"
+  private static final ANDROID_PACKAGE_DEBUG_TASK_NAME = "androidPackageDebug"
+  private static final ANDROID_PACKAGE_RELEASE_TASK_NAME = "androidPackageRelease"
+  private static final ANDROID_INSTALL_TASK_NAME = "androidInstall"
+  private static final ANDROID_UNINSTALL_TASK_NAME = "androidUninstall"
+  
   private static final PROPERTIES_FILES = ['local', 'build', 'default']
   private static final ANDROID_JARS = ['anttasks', 'sdklib', 'androidprefs', 'apkbuilder', 'jarutils']
-
+  
   private androidConvention
   private sdkDir
   private toolsDir
-
+  
+  private Project project
+  
+  private androidProcessResourcesTask, proguardTask, androidPackageDebugTask, androidPackageReleaseTask,
+  androidInstallTask, androidUninstallTask
+  
   boolean verbose = false
-
+  
   @Override
   public void use(Project project, ProjectPluginsContainer plugins) {
     JavaPlugin javaPlugin = plugins.usePlugin(JavaPlugin.class, project);
-
+    
+    this.project = project
+    
     androidConvention = new AndroidPluginConvention(project)
     project.convention.plugins.android = androidConvention
-
-    androidSetup(project)
-    defineTasks(project)
-    configureCompile(project)
+    
+    androidSetup()
+    defineTasks()
+    configureCompile()
   }
-
-  private void androidSetup(Project project) {
+  
+  private void androidSetup() {
     def ant = project.ant
-
+    
     PROPERTIES_FILES.each { ant.property(file: "${it}.properties") }
     sdkDir = project['sdk.dir']
     toolsDir = new File(sdkDir, "tools")
-
+    
     ant.path(id: 'android.antlibs') {
       ANDROID_JARS.each { pathelement(path: "${sdkDir}/tools/lib/${it}.jar") }
     }
-
+    
     ant.condition('property': "exe", value: ".exe", 'else': "") { os(family: "windows") }
     ant.property(name: "adb", location: new File(toolsDir, "adb${ant['exe']}"))
     ant.property(name: "zipalign", location: new File(toolsDir, "zipalign${ant['exe']}"))
     ant.property(name: 'adb.device.arg', value: '')
-
+    
     def outDir = project.buildDir.absolutePath
     ant.property(name: "out.debug.unaligned.package", location: "${outDir}/${project.name}-debug-unaligned.apk")
     ant.property(name: "out.debug.package", location: "${outDir}/${project.name}-debug.apk")
     ant.property(name: "out.unsigned.package", location: "${outDir}/${project.name}-unsigned.apk")
     ant.property(name: "out.unaligned.package", location: "${outDir}/${project.name}-unaligned.apk")
     ant.property(name: "out.release.package", location: "${outDir}/${project.name}-release.apk")
-
+    
     ant.taskdef(name: 'setup', classname: 'com.android.ant.SetupTask', classpathref: 'android.antlibs')
-
+    
     // The following properties are put in place by the setup task:
     // android.jar, android.aidl, aapt, aidl, and dx
     ant.setup('import': false)
-
+    
     ant.taskdef(name: "aaptexec", classname: "com.android.ant.AaptExecLoopTask", classpathref: "android.antlibs")
     ant.taskdef(name: "apkbuilder", classname: "com.android.ant.ApkBuilderTask", classpathref: "android.antlibs")
   }
-
-  private void defineTasks(Project project) {
-    ProcessAndroidResources processAndroidResourcesTask = project.tasks.add(GENERATE_SOURCES_TASK_NAME, ProcessAndroidResources.class)
-    processAndroidResourcesTask.description = "Generate R.java source file from Android resource XML files"
-    project.tasks.compileJava.dependsOn(processAndroidResourcesTask)
-
-    ProGuard proguardTask = project.tasks.add(PROGUARD_TASK_NAME, ProGuard.class)
+  
+  private void defineTasks() {
+    defineAndroidProcessResourcesTask()
+    defineProguardTask()
+    defineAndroidPackageDebugTask()
+    defineAndroidPackageReleaseTask()
+    defineAndroidInstallTask()
+    defineAndroidUninstallTask()
+    defineTaskDependencies()
+  }
+  
+  private void defineAndroidProcessResourcesTask() {
+    androidProcessResourcesTask = project.tasks.add(ANDROID_PROCESS_RESOURCES_TASK_NAME, ProcessAndroidResources.class)
+    androidProcessResourcesTask.description = "Generate R.java source file from Android resource XML files"
+  }
+  
+  private void defineProguardTask() {
+    proguardTask = project.tasks.add(PROGUARD_TASK_NAME, ProGuard.class)
     proguardTask.description = "Process classes and JARs with ProGuard"
-    proguardTask.dependsOn('classes')
-
-    // Since there is no configuration for this task, just define it inline.
-    project.task(ANDROID_PACKAGE_DEBUG_TASK_NAME) << {
-
+  }
+  
+  private void defineAndroidPackageDebugTask() {
+    androidPackageDebugTask = project.task(ANDROID_PACKAGE_DEBUG_TASK_NAME) << {
       ant.apply(executable: ant.dx, failonerror: true, parallel: true) {
         arg(value: "--dex")
         arg(value: "--output=${androidConvention.intermediateDexFile}")
@@ -124,26 +111,26 @@ class AndroidPlugin implements Plugin {
         arg(path: project.sourceSets.main.classesDir)
         fileset(dir: project.buildDir, includes: "*.jar")
       }
-
+      
       ant.aaptexec(executable: ant.aapt,
-                   command: 'package',
-                   manifest: androidConvention.androidManifest.path,
-                   resources: androidConvention.resDir.path,
-                   assets: androidConvention.assetsDir,
-                   androidjar: ant['android.jar'],
-                   outfolder: project.buildDir,
-                   basename: project.name)
-
+          command: 'package',
+          manifest: androidConvention.androidManifest.path,
+          resources: androidConvention.resDir.path,
+          assets: androidConvention.assetsDir,
+          androidjar: ant['android.jar'],
+          outfolder: project.buildDir,
+          basename: project.name)
+      
       ant.apkbuilder(outfolder: project.buildDir,
-                     basename: project.name,
-                     signed: true,
-                     'verbose': verbose) {
-        ant.file(path: androidConvention.intermediateDexFile.absolutePath)
-        //sourcefolder(path: project.sourceSets.main.java)
-        nativefolder(path: androidConvention.nativeLibsDir)
-        //jarfolder(path: androidConvention.nativeLibsDir)
-      }
-
+          basename: project.name,
+          signed: true,
+          'verbose': verbose) {
+            ant.file(path: androidConvention.intermediateDexFile.absolutePath)
+            //sourcefolder(path: project.sourceSets.main.java)
+            nativefolder(path: androidConvention.nativeLibsDir)
+            //jarfolder(path: androidConvention.nativeLibsDir)
+          }
+      
       ant.exec(executable: ant.zipalign, failonerror: true) {
         if (verbose) arg(line: '-v')
         arg(value: '-f')
@@ -152,9 +139,20 @@ class AndroidPlugin implements Plugin {
         arg(path: new File(project.buildDir, "${project.name}-debug.apk"))
       }
     }
-    project.tasks[ANDROID_PACKAGE_DEBUG_TASK_NAME].dependsOn(proguardTask)
-
-    project.task(ANDROID_INSTALL_TASK_NAME) << {
+    androidPackageDebugTask.description =
+        "Creates the Android application apk package, signed with debug key"
+  }
+  
+  private void defineAndroidPackageReleaseTask() {
+    androidPackageReleaseTask = project.task(ANDROID_PACKAGE_RELEASE_TASK_NAME) << {
+      
+    }
+    androidPackageReleaseTask.description =
+        "Creates the Android application apk package, which must be signed before it is published"
+  }
+  
+  private void defineAndroidInstallTask() {
+    androidInstallTask = project.task(ANDROID_INSTALL_TASK_NAME) << {
       ant.exec(executable: ant['adb'], failonerror: true) {
         arg(line: ant['adb.device.arg'])
         arg(value: 'install')
@@ -162,14 +160,31 @@ class AndroidPlugin implements Plugin {
         arg(path: ant['out.debug.package'])
       }
     }
-    project.tasks[ANDROID_INSTALL_TASK_NAME].dependsOn(ANDROID_PACKAGE_DEBUG_TASK_NAME)
-    project.tasks.assemble.dependsOn(ANDROID_PACKAGE_DEBUG_TASK_NAME)
+    androidInstallTask.description =
+        "Installs the debug package onto a running emulator or device"
   }
-
-  private void configureCompile(Project project) {
+  
+  private void defineAndroidUninstallTask() {
+    androidUninstallTask = project.task(ANDROID_UNINSTALL_TASK_NAME) << {
+      
+    }
+    androidUninstallTask.description =
+        "Uninstalls the application from a running emulator or device"
+  }
+  
+  private void defineTaskDependencies() {
+    project.tasks.compileJava.dependsOn(androidProcessResourcesTask)
+    proguardTask.dependsOn('classes')
+    androidPackageDebugTask.dependsOn(proguardTask)
+    project.tasks.assemble.dependsOn(ANDROID_PACKAGE_DEBUG_TASK_NAME)
+    androidInstallTask.dependsOn(ANDROID_PACKAGE_DEBUG_TASK_NAME)
+  }
+  
+  private void configureCompile() {
     def mainSource = project.tasks.compileJava.source
     project.tasks.compileJava.source = [androidConvention.genDir, mainSource]
-    project.sourceSets.main.compileClasspath += project.files(project.ant.references['android.target.classpath'].list())
+    project.sourceSets.main.compileClasspath +=
+        project.files(project.ant.references['android.target.classpath'].list())
     project.compileJava.options.bootClasspath = project.ant.references['android.target.classpath']
   }
 }
