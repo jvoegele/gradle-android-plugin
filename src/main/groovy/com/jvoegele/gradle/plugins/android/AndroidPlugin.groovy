cd9fb7ea14b95936df48ad2e5cf7aae88ac880ee
Jason Voegele <jason@jvoegele.com>
2010-05-01 07:51:21 -0400

    Assemble APK from ProGuard-processed JAR.

--- src/main/groovy/com/jvoegele/gradle/plugins/android/AndroidPlugin.groovy
+++ src/main/groovy/com/jvoegele/gradle/plugins/android/AndroidPlugin.groovy
@@ -225,8 +225,8 @@ class AndroidPlugin implements Plugin<Project> {
       arg(value: "--dex")
       arg(value: "--output=${androidConvention.intermediateDexFile}")
       if (verbose) arg(line: "--verbose")
-      arg(path: project.sourceSets.main.classesDir)
-      fileset(dir: project.buildDir, includes: "*.jar")
+      //arg(path: project.sourceSets.main.classesDir)
+      fileset(file: proguardTask.outJar)
     }
 
     logger.info("Packaging resources")
