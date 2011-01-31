ebfd24ae9bda55ffa8fd0eff938a59969fae18be
Matthias Käppler <m.kaeppler@gmail.com>
2011-01-31 13:38:36 +0100

    only check stdout for build failures; turn test failure check into a positive check (i.e. test for success), since the ADB tool currently reports errors via stdout.

--- src/main/groovy/com/jvoegele/gradle/tasks/android/InstrumentationTestsTask.groovy
+++ src/main/groovy/com/jvoegele/gradle/tasks/android/InstrumentationTestsTask.groovy
@@ -1,6 +1,7 @@
 package com.jvoegele.gradle.tasks.android
 
 import com.jvoegele.gradle.tasks.android.exceptions.InstrumentationTestsFailedException;
+import com.jvoegele.gradle.tasks.android.exceptions.AdbErrorException;
 
 class InstrumentationTestsTask extends AdbExec {
 
@@ -17,10 +18,35 @@ class InstrumentationTestsTask extends AdbExec {
   @Override
   def checkForErrors(stream) {
     def reader = new InputStreamReader(new ByteArrayInputStream(stream.toByteArray()))
+    if (stream == stdout) {
+      detectBuildFailures(reader)
+    } else { // stderr
+      detectAdbErrors(reader);
+    }
+  }
+  
+  private void detectBuildFailures(def reader) {
+    def success = false
+    // ADB currently fails with errors on stdout, so we literally have to check
+    // for 'OK' to 
     reader.eachLine {
-      if (it.matches("FAILURES!!!")) {
-        throw new InstrumentationTestsFailedException();
+      if (it.matches("^OK \\(([0-9]+ tests){1}\\)")) {
+        success = true
+        return
       }
+    }
+    
+    if (!success) {
+      throw new InstrumentationTestsFailedException();
+    }
+  }
+  
+  // at the moment, ADB errors are actually reported via stdout instead of stderr.
+  // this method may become useful though once Google fixes that.
+  private void detectAdbErrors(def reader) {
+    if (reader.getText().trim()) {
+      // at the moment, we treat any non-empty text on stderr as an error
+      throw new AdbErrorException("There was an error while trying to run the instrumentation tests")
     }
   }
 }
