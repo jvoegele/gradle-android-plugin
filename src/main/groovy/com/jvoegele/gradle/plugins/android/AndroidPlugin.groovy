d6966cd244cc4f2d3738eb102d58d4dfb5559794
Ladislav Thon <ladicek@gmail.com>
2011-03-05 00:04:27 +0100

    set group for InstrumentTask

--- src/main/groovy/com/jvoegele/gradle/plugins/android/AndroidPlugin.groovy
+++ src/main/groovy/com/jvoegele/gradle/plugins/android/AndroidPlugin.groovy
@@ -201,6 +201,7 @@ class AndroidPlugin implements Plugin<Project> {
         ANDROID_INSTRUMENT_TASK_NAME,
         description: description,
         type: InstrumentationTestsTask)
+    androidInstrumentTask.group = ANDROID_GROUP
   }
 
   private void defineTaskDependencies() {
