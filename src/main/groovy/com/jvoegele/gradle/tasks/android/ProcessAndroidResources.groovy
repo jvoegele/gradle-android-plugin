134ba7d0dce0a5dd563c081e90049e9fdcc6620b
Fabio Da Soghe <think01@mousedogstudios.com>
2011-03-04 10:53:16 +0000

    Fix for AIDL problem (see http://code.google.com/p/gradle-android-plugin/issues/detail?id=4 )

--- src/main/groovy/com/jvoegele/gradle/tasks/android/ProcessAndroidResources.groovy
+++ src/main/groovy/com/jvoegele/gradle/tasks/android/ProcessAndroidResources.groovy
@@ -1,46 +1,62 @@
 package com.jvoegele.gradle.tasks.android;
 
-import groovy.lang.MetaClass;
+import groovy.lang.MetaClass
 
-import org.gradle.api.internal.ConventionTask;
-import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.file.FileTree
+import org.gradle.api.internal.ConventionTask
+import org.gradle.api.tasks.TaskAction
 
-import com.jvoegele.gradle.plugins.android.AndroidPluginConvention;
+import com.jvoegele.gradle.plugins.android.AndroidPluginConvention
 
 class ProcessAndroidResources extends ConventionTask {
   boolean verbose
 
   AndroidPluginConvention androidConvention
   File genDir
+  
+  // Define which are the AIDL files (assuming they are created in the main sourceSet, 
+  // and that we have only one main src directory).
+  String aidlDir = project.sourceSets.main.java.srcDirs.iterator().next().getAbsolutePath() 
+  FileTree aidlFiles = project.fileTree {
+    from aidlDir
+    include "**/*.aidl"
+  }
 
   public ProcessAndroidResources () {
     super()
-    
+
     androidConvention = project.convention.plugins.android
     genDir = androidConvention.genDir
-    
+
     // Set input and output files and directories for this task
     inputs.file (androidConvention.androidManifest.absolutePath)
+    inputs.files (aidlFiles)
     inputs.dir (androidConvention.resDir.absolutePath)
     outputs.dir (genDir.absolutePath)
   }
-  
-  
+
+
   @TaskAction
   protected void process() {
     genDir.mkdirs()
-	project.logger.info("Generating AIDL java files")
-	project.ant.exec(executable: ant.aidl, failonerror: "true") {
-		arg(value: '-p')
-		arg(path: ant.references['android.aidl'])
-		arg(value: '-I')
-		arg(path: project.tasks.compileJava.source.absolutePath)
-		arg(value: '-o')
-		arg(path: genDir.absolutePath)
-		fileset(dir: 'src'){
-			include(name: '** /*.aidl')
-		}
-	}
+
+    // Check if there is at least one AIDL file
+    if (!aidlFiles.isEmpty()) {
+      project.logger.info("Generating AIDL java files...")
+      project.ant.apply(executable: ant.aidl, failonerror: "true") {
+        arg(value: "-I${aidlDir}")
+        arg(value: "-o${genDir.absolutePath}")
+        fileset(dir: aidlDir) {
+          include(name: '**/*.aidl')
+        }
+
+        // Note by Fabio: android.aidl is empty and the -p option wants a "preprocess" file in input, which
+        // I don't know what is or where came from, so I omitted.       
+//        arg(value: '-p')
+//        arg(path: ant.references['android.aidl'])
+      }
+    }
+
     project.logger.info("Generating R.java / Manifest.java from the resources...")
     project.ant.exec(executable: ant.aapt, failonerror: "true") {
       arg(value: "package")
@@ -55,14 +71,5 @@ class ProcessAndroidResources extends ConventionTask {
       arg(value: "-I")
       arg(path: ant['android.jar'])
     }
-
-    /* TODO: include command to process .aidl files, as done by this Ant task:
-     <apply executable="${aidl}" failonerror="true">
-     <arg value="-p${android.aidl}" />
-     <arg value="-I${source.absolute.dir}" />
-     <arg value="-o${gen.absolute.dir}" />
-     <fileset dir="${source.absolute.dir}">
-     <include name="** /*.aidl" />
-     */
   }
 }
