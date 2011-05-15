26a4eceddcc45d122e05f7cffce8b1b8f210b722
Felix Schulze <code@felixschulze.de>
2011-05-15 13:11:29 +0200

    Get sdk.dir from project.ant

--- src/main/groovy/com/jvoegele/gradle/tasks/android/EmulatorTask.groovy
+++ src/main/groovy/com/jvoegele/gradle/tasks/android/EmulatorTask.groovy
@@ -3,21 +3,17 @@ package com.jvoegele.gradle.tasks.android
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.TaskAction
-import org.gradle.api.internal.ConventionTask
 
 class EmulatorTask extends DefaultTask {
 	
 	@Input public String avdName
-	private sdkDir
 
-	def EmulatorTask() {
-		sdkDir = System.getenv("ANDROID_HOME")
-	}
+	def EmulatorTask() {}
 
 	@TaskAction
 	def start() {
 		project.logger.info("Starting emulator...")
-		def command = sdkDir + "/tools/emulator -avd " +avdName
+		def command = project.ant['sdk.dir'] + "/tools/emulator -avd " +avdName
 		def proc = command.execute()
 	}
 }
