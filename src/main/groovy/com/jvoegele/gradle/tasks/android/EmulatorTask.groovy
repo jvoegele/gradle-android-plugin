0606046214b45d72ca9984bbdc73b7a78a2d1094
Felix Schulze <code@felixschulze.de>
2011-05-15 12:16:27 +0200

    Start emulator with gradle

---
+++ src/main/groovy/com/jvoegele/gradle/tasks/android/EmulatorTask.groovy
@@ -0,0 +1,23 @@
+package com.jvoegele.gradle.tasks.android
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.internal.ConventionTask
+
+class EmulatorTask extends ConventionTask {
+	
+	@Input public String avdName
+	private sdkDir
+
+	def EmulatorTask() {
+		sdkDir = System.getenv("ANDROID_HOME")
+	}
+
+	@TaskAction
+	def start() {
+		project.logger.info("Starting emulator...")
+		def command = sdkDir + "/tools/emulator -avd " +avdName
+		def proc = command.execute()
+	}
+}
