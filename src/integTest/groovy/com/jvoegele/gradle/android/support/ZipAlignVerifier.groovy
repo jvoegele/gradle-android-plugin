9c6542713585c1c87d571b4f82b0de4ab7bf65c2
Ladislav Thon <ladicek@gmail.com>
2011-07-30 15:31:27 +0200

    removed asserts for non-zipaligned archives in integration tests
    I originally added some asserts into integration tests of debug builds
    for testing that the *-unaligned.apk were actually not zipaligned. This
    should have never happened, because even if zipalign isn't ran, the resulintg
    archive can still be effectively "zipaligned" by accident. The tests were
    passing back then, but aren't right now for me, which suggests that this
    is environment-specific.

--- src/integTest/groovy/com/jvoegele/gradle/android/support/ZipAlignVerifier.groovy
+++ src/integTest/groovy/com/jvoegele/gradle/android/support/ZipAlignVerifier.groovy
@@ -2,20 +2,12 @@ package com.jvoegele.gradle.android.support
 
 import org.gradle.util.OperatingSystem
 
-import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertTrue
 
 class ZipAlignVerifier {
   def project
 
   def verifyAligned(archive) {
-    doVerify archive, true
-  }
-
-  def verifyNotAligned(archive) {
-    doVerify archive, false
-  }
-
-  def doVerify(archive, expectedResult) {
     def androidTools = new File(System.getenv("ANDROID_HOME"), "tools")
     def zipalign = new File(androidTools, "zipalign${OperatingSystem.current().isWindows() ? '.exe' : ''}")
 
@@ -25,7 +17,6 @@ class ZipAlignVerifier {
       ignoreExitValue = true
     }
 
-    assertEquals("The archive ${archive} ${expectedResult ? 'must' : 'must NOT'} be zipaligned",
-            expectedResult, result.exitValue == 0)
+    assertTrue("The archive ${archive} must be zipaligned", result.exitValue == 0)
   }
 }
