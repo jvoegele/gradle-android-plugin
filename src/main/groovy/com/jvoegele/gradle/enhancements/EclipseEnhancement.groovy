25d86d34d0763ec8b3a8d6030870fa4a09a0d6de
Matthias Kaeppler <m.kaeppler@gmail.com>
2011-07-30 17:19:06 +0200

    migrate EclipseEnhancement to new Gradle 1.0 Eclipse DSL

--- src/main/groovy/com/jvoegele/gradle/enhancements/EclipseEnhancement.groovy
+++ src/main/groovy/com/jvoegele/gradle/enhancements/EclipseEnhancement.groovy
@@ -14,24 +14,20 @@ class EclipseEnhancement extends GradlePluginEnhancement {
       if (!project.plugins.hasPlugin('eclipse'))
         return;
 
-      project.configure(project.eclipseProject) {
-        beforeConfigured {
-          natures 'com.android.ide.eclipse.adt.AndroidNature'
-          def builders = new LinkedList(buildCommands)
-          builders.addFirst(new BuildCommand('com.android.ide.eclipse.adt.PreCompilerBuilder'))
-          builders.addFirst(new BuildCommand('com.android.ide.eclipse.adt.ResourceManagerBuilder'))
-          builders.addLast(new BuildCommand('com.android.ide.eclipse.adt.ApkBuilder'))
-          buildCommands = new ArrayList(builders)
-        }
+      project.eclipse.project {
+        natures 'com.android.ide.eclipse.adt.AndroidNature'
+        def builders = new LinkedList(buildCommands)
+        builders.addFirst(new BuildCommand('com.android.ide.eclipse.adt.PreCompilerBuilder'))
+        builders.addFirst(new BuildCommand('com.android.ide.eclipse.adt.ResourceManagerBuilder'))
+        builders.addLast(new BuildCommand('com.android.ide.eclipse.adt.ApkBuilder'))
+        buildCommands = new ArrayList(builders)
       }
 
-      project.configure(project.eclipseClasspath) {
-        beforeConfigured {
-          containers.removeAll { it == 'org.eclipse.jdt.launching.JRE_CONTAINER' }
-          containers 'com.android.ide.eclipse.adt.ANDROID_FRAMEWORK'
-          sourceSets = project.sourceSets
-          sourceSets.main.java.srcDir 'gen'
-        }
+      project.eclipse.classpath {
+        containers.removeAll { it == 'org.eclipse.jdt.launching.JRE_CONTAINER' }
+        containers 'com.android.ide.eclipse.adt.ANDROID_FRAMEWORK'
+        sourceSets = project.sourceSets
+        sourceSets.main.java.srcDir 'gen'
       }
     }
   }
